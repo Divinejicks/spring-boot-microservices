df8459ecc1194ed5371a055ee3ef73636259beea
Divinejicks <divinejicks@gmail.com>
2018-03-21 11:36:04 +0100

    added the api controller class and the application.properties file

--- counterservice/src/main/java/com/divinejicks/counterservice/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/divinejicks/counterservice/counterservice/api/CounterController.java
@@ -1,5 +1,6 @@
 package com.divinejicks.counterservice.counterservice.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class CounterController {
 
-    private static AtomicLong count = new AtomicLong(0);
+    private static AtomicLong count = new AtomicLong(0L);
+
+    @Value(value = "${counter.prefixMessage}")
+    private String prefixMessage;
 
     @GetMapping(path = "/count")
-    public long getCount(){
-        return count.getAndIncrement();
+    public String getCount(){
+        System.out.println(prefixMessage);
+        return prefixMessage + count.getAndIncrement();
     }
 }
